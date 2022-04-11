62cb60241ada6890568b81b115a756bfcbffe6a5
sota <sota1210nittc@gmail.com>
2022-04-11 13:05:11 +0900

    update disp

--- procon33/Main.cpp
+++ procon33/Main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <Siv3D.hpp> // OpenSiv3D v0.6.3
 
-#include "analyze.hpp"
+#include "form.hpp"
 
 
 void Main(){
@@ -12,6 +12,7 @@ void Main(){
 	// 再生位置の変更の有無
 	bool hasChanged = false;
 	bool is_stopping = false;
+	bool time_changed = false;
 	while(System::Update()){
 		// 再生・演奏時間
 		const String time = FormatTime(SecondsF(audio.posSec()), U"M:ss")
@@ -20,10 +21,12 @@ void Main(){
 		double progress = static_cast<double>(audio.posSample()) / audio.samples();
 
 		ClearPrint();
-		if(!is_stopping){
+		if(!is_stopping || time_changed){
 			// FFT 解析
 			FFT::Analyze(fft, audio);
 		}
+		time_changed = false;
+
 		// 結果を可視化
 		Array<std::pair<double,int>> loudness(800);
 		for(int i = 0; i < 800; i++){
@@ -40,15 +43,16 @@ void Main(){
 		}
 		// 目盛りの表示
 		for(int i = 0; i < 800; i += 40){
-			font(U"{}"_fmt((int)fft.resolution * i)).draw(i, 480);
+			font(U"{}"_fmt((int)(fft.resolution * i))).draw(i, 480);
 		}
 		// 周波数表示
 		Rect(Cursor::Pos().x, 0, 1, Scene::Height()).draw();
 		Print << U"{} Hz"_fmt(Cursor::Pos().x * fft.resolution);
 
 		// Start, Pause Button
-		if(SimpleGUI::Button(U"Play/Pause", Vec2(40, 500), 120, (bool)audio)){
-			is_stopping = audio.isPlaying();
+		const String button_name = audio.isPlaying() ? U"Pause" : U"Play";
+		if(!hasChanged) is_stopping = !audio.isPlaying();
+		if(SimpleGUI::Button(button_name, Vec2(40, 500), 120, (bool)audio) || (audio && KeySpace.down())){
 			if(!audio.isPlaying()){
 				audio.play();
 			}else{
@@ -60,12 +64,29 @@ void Main(){
 		if(SimpleGUI::Button(U"Open", Vec2(300, 500), 120)){
 			audio.stop(0.5s);
 			audio = Dialog::OpenAudio();
-			audio.play();
 		}
 
+		// slider sample time
+		if(KeyComma.down()){
+			audio.seekTime(audio.posSec() - 1.0/30);
+			time_changed = true;
+		}
+		if(KeyPeriod.down()){
+			audio.seekTime(audio.posSec() + 1.0/30);
+			time_changed = true;
+		}
+		// slider 3s
+		if(KeyLeft.down()){
+			audio.seekTime(audio.posSec() - 3);
+			time_changed = true;
+		}
+		if(KeyRight.down()){
+			audio.seekTime(audio.posSec() + 3);
+			time_changed = true;
+		}
 		// スライダー
 		if(SimpleGUI::Slider(time, progress, Vec2(40, 540), 130, 590, !audio.isEmpty())){
-			audio.pause(0.1s);
+			audio.pause(0.5s);
 			// 再生位置を変更
 			audio.seekSamples(static_cast<int64>(audio.samples() * progress));
 			// ノイズを避けるため、スライダーから手を離すまで再生は再開しない
