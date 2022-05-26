ab56cb9f406f011ec4e3da83526a004fe323ce64
sota <sota1210nittc@gmail.com>
2022-05-26 13:22:49 +0900

    add audio merge, audio separate

--- audio/changehz.cpp
+++ audio/changehz.cpp
@@ -2,14 +2,25 @@
 #define rep(i, n) for(int i = 0; i < (n); i++)
 using std::vector;
 
+constexpr int short_max = (1 << 15) - 1;
+
 //URL: https://hwswsgps.hatenablog.com/entry/2018/08/19/172401
-struct WAV_PRM {
+struct Wave {
   int fs; //サンプリング周波数
   int bits; //量子化bit数
-  int L; //データ長
+  int L = 0; //データ長
+  vector<int> data;
+  int &operator[](const int &i) noexcept{
+    assert(0 <= i && i < L);
+    return data[i];
+  }
+  const int &operator[](const int &i) const noexcept{
+    assert(0 <= i && i < L);
+    return data[i];
+  }
 };
 
-vector<int> audio_read(WAV_PRM &prm, char *filename){
+void audio_read(Wave &prm, const char *filename){
   char tmp[24];
 
   long fmt_samples_per_sec;
@@ -34,17 +45,15 @@ vector<int> audio_read(WAV_PRM &prm, char *filename){
   prm.L = data_size / 2;
 
   //音声データ代入
-  vector<int> data(prm.L);
+  prm.data.resize(prm.L);
   for(int i = 0; i < prm.L; i++){
     short data_data;
     fread(&data_data, 2, 1, fp);
-    data[i] = data_data;
+    prm[i] = data_data;
   }
-
   fclose(fp);
-  return data;
 }
-void audio_write(const vector<int> &data, const WAV_PRM &prm, char *filename){
+void audio_write(const Wave &prm, const char *filename){
   FILE *fp = fopen(filename, "wb");
   assert(fp != NULL);
 
@@ -80,20 +89,16 @@ void audio_write(const vector<int> &data, const WAV_PRM &prm, char *filename){
   for(int i = 0; i < prm.L; i++){
     short data_data;
     //リミッター
-    if(data[i] > 32767){
-      data_data = 32767;
-    }else if(data[i] < -32767){
-      data_data = -32767;
-    }else{
-      data_data = data[i];
-    }
+    if(prm[i] > short_max) data_data = short_max;
+    else if(prm[i] < -short_max) data_data = -short_max;
+    else data_data = prm[i];
     fwrite(&data_data, 2, 1, fp);
   }
   fclose(fp);
 }
 
 //prm.fs[Hz] -> fs[Hz]
-vector<int> change_sampling_hz(const vector<int> &data, WAV_PRM &prm, int fs){
+void change_sampling_hz(Wave &prm, int fs){
   assert(prm.fs % fs == 0);
   const int p = prm.fs / fs;
   prm.fs = fs;
@@ -101,20 +106,64 @@ vector<int> change_sampling_hz(const vector<int> &data, WAV_PRM &prm, int fs){
   vector<int> new_data(prm.L);
   rep(i, prm.L){
     int tot = 0;
-    //rep(j, p) tot += data[i*p + j];
+    //rep(j, p) tot += prm[i*p + j];
     //new_data[i] = tot / p;
-    new_data[i] = data[i * p];
+    new_data[i] = prm[i * p];
   }
-  return new_data;
+  prm.data = std::move(new_data);
 }
 
+// st[i]: waves[i]の開始位置(1sample単位)
+[[nodiscard]]
+Wave merge_audio(const vector<Wave> &waves, const vector<int> &st){
+  Wave res;
+  res.bits = waves[0].bits;
+  res.fs = waves[0].fs;
+  res.L = 0;
+  const int n = waves.size();
+  for(int i = 0; i < n; i++){
+    assert(res.bits == waves[i].bits && res.fs == waves[i].fs);
+    res.L = std::max(res.L, waves[i].L + st[i]);
+  }
+  res.data.resize(res.L);
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < waves[i].L; j++){
+      res[j + st[i]] += waves[i][j];
+    }
+  }
+  for(int i = 0; i < res.L; i++){
+    if(res[i] > short_max) res[i] = short_max;
+    else if(res[i] < -short_max) res[i] = -short_max;
+  }
+  return res;
+}
 
-//a.exe [import file name] [export file name]
+//sep[i]: 分割する位置。sep.front() = 0, sep.back() = wave.L
+[[nodiscard]]
+vector<Wave> separate_audio(const Wave &wave, const vector<int> &sep){
+  const int n = sep.size();
+  assert(sep[0] == 0 && sep[n - 1] == wave.L);
+  for(int i = 1; i < n - 1; i++){
+    assert(sep[i] < sep[i + 1]);
+    assert(0 < sep[i] && sep[i] < wave.L);
+  }
+  vector<Wave> res(n - 1);
+  for(int i = 0; i < n - 1; i++){
+    res[i].bits = wave.bits;
+    res[i].fs = wave.fs;
+    for(int j = sep[i]; j < sep[i + 1]; j++){
+      res[i].data.emplace_back(wave[j]);
+    }
+    assert(res[i].data.size() == sep[i + 1] - sep[i]);
+  }
+  return res;
+}
+
+//a.exe [import file name] [export file name] [sampling hz]
 int main(int argc, char *args[]){
-  WAV_PRM prm;
-  vector<int> data = audio_read(prm, args[1]);
+  Wave prm;
+  audio_read(prm, args[1]);
   printf("bits:%d, sampling hz:%d\n", prm.bits, prm.fs);
-  vector<int> new_data = change_sampling_hz(data, prm, 12000);
-  audio_write(new_data, prm, args[2]);
-  printf("completed\n");
+  change_sampling_hz(prm, std::stoi(args[3]));
+  audio_write(prm, args[2]);
 }
