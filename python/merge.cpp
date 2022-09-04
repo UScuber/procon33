f9fd0cfed0ac1e1baabff30ea74135a6724749ac
sota <sota1210nittc@gmail.com>
2022-09-04 16:53:38 +0900

    delete python directory

--- python/merge.cpp
+++
@@ -1,9 +0,0 @@
-#include "../analyze/audio/wave.hpp"
-
-int main(){
-  Wave wave,wave2;
-  read_audio(wave, "audio/JKspeech/J01.wav");
-  read_audio(wave2, "audio/JKspeech/J02.wav");
-  Wave res = merge_audio({ wave, wave2 }, { 0, 0 });
-  write_audio(res, "twice.wav");
-}
