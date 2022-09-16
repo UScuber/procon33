3b3f7af9c2db3e5dfdb79327debc30366ed70460
tamagosushi <mitachi.ts75@gmail.com>
2022-09-16 16:03:43 +0900

    受け取る分割データ数を常に最大にするよう変更

--- net/submit.py
+++ net/submit.py
@@ -1,9 +1,13 @@
 import requests
 import sys
 import json
+import os
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+urllib3.disable_warnings(InsecureRequestWarning)
+
 
 URL = "https://procon33-practice.kosen.work"
-select = 2
 TOKEN = "3fc3169361f5daa766a8a3e3f757aaa20de7d4c7dafa3816ee4f7de4b9c71730"
 query = "?token=" + TOKEN
 
@@ -25,7 +29,7 @@ def alert(j):
 
 """/matchにGETリクエストを送って、試合情報のjsonを返す"""
 def match_get():
-    res_match = requests.get(URL + "/match" + query)
+    res_match = requests.get(URL + "/match" + query, verify=False)
     alert(res_match)
     print(res_match.text)
     return res_match.text
@@ -33,15 +37,18 @@ def match_get():
 
 """/problemにGETリクエストを送って、問題情報のjsonを返す"""
 def problem_get():
-    res_problem = requests.get(URL + "/problem" + query)
+    res_problem = requests.get(URL + "/problem" + query, verify=False)
     alert(res_problem)
     print(res_problem.text)
-    return res_problem.text
+    # 受けとったjsonを辞書型に変換
+    problem_dic = json.loads(res_problem.text)
+    # 変換したjsonから選択数を抽出する
+    return int(problem_dic["chunks"])
 
 
 """/problem/chunksに分割数をPOSTリクエストで送って、各分割データのファイル名の配列のjsonを返す"""
-def chunks_post():
-    res_chunks = requests.post(URL + "/problem/chunks" + query + "&n=" + str(select))
+def chunks_post(select):
+    res_chunks = requests.post(URL + "/problem/chunks" + query + "&n=" + str(select), verify=False)
     alert(res_chunks)
     print(res_chunks.text)
     return res_chunks.text
@@ -51,21 +58,26 @@ def chunks_post():
 def file_get(res_chunks):
     # ファイル名配列のjsonを辞書型に変換する
     filenames = json.loads(res_chunks)
+    # audioディレクトリに移動
+    os.chdir("audio")
     # ファイル名配列の長さ分だけfor分を回し、それぞれに対応するwavファイルをダウンロードする
     for i in range(len(filenames["chunks"])):
         # ファイル名配列のi番目にあるファイルにGETリクエストを送る
-        res_wav = requests.get(URL + "/problem/chunks/" + (filenames["chunks"])[i] + query)
-        # wavファイルをダウンロード
+        res_wav = requests.get(URL + "/problem/chunks/" + (filenames["chunks"])[i] + query, verify=False)
+        # audioディレクトリにwavファイルをダウンロード
         with open((filenames["chunks"])[i], "wb") as saveFile:
             saveFile.write(res_wav.content)
+    # ディレクトリを元に戻す
+    os.chdir("..")
 
 
 """/problemに回答のjsonをPOSTリクエストで送り、返ってきたjsonファイルを受け取る"""
-
+def problem_post():
+    res_problem = requests.post(URL + "/problem" + query, json.dump({}), headers={'Content-Type': 'application/json'}, verify=False)
 
 
 
 if __name__ == "__main__":
     match_get()
-    problem_get()
-    file_get(chunks_post())
+    select = problem_get()
+    file_get(chunks_post(select))
