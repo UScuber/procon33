59607a04a5fa34586bd657a101bb8641b4390b0a
tamagosushio <mitachi.ts75@gmail.com>
2022-09-17 21:59:04 +0900

    ひとまず完成
    ・受け取ったjsonをファイルとして出力するよう変更
    ・分割音声ファイルの取得と結合を一度にできるよう変更
    ・jsonファイルを提出できるよう変更

--- net/submit.py
+++ net/submit.py
@@ -1,10 +1,18 @@
+#cording utf-8
+
+from re import sub
 import requests
 import sys
 import json
 import os
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
+
+import cat
 urllib3.disable_warnings(InsecureRequestWarning)
+
+# 自作モジュール
+import cat
 
 
 URL = "https://procon33-practice.kosen.work"
@@ -27,11 +35,22 @@ def alert(j):
             sys.exit()
 
 
+
+"""jsonファイルを作成する"""
+def make_json(file_name, text):
+    # 受け取った文字列を辞書型に変換
+    dictionary = json.loads(text)
+    with open(file_name, "w") as j:
+        json.dump(dictionary, j, indent=2)
+
+
+
+
 """/matchにGETリクエストを送って、試合情報のjsonを返す"""
 def match_get():
     res_match = requests.get(URL + "/match" + query, verify=False)
     alert(res_match)
-    print(res_match.text)
+    make_json("match.json", res_match.text)
     return res_match.text
 
 
@@ -39,12 +58,15 @@ def match_get():
 def problem_get():
     res_problem = requests.get(URL + "/problem" + query, verify=False)
     alert(res_problem)
-    print(res_problem.text)
+    make_json("problem.json", res_problem.text)
     # 受けとったjsonを辞書型に変換
     problem_dic = json.loads(res_problem.text)
     # 札数を../analyze/testのinformation.txtに書き込む
     with open("../analyze/test/information.txt", "w") as txt:
         print(problem_dic["data"], file=txt)
+    # 問題IDをproblem.txtに書き込む
+    with open("problem.txt", "w") as txt:
+        print(problem_dic["id"], file=txt)
     # 変換したjsonから選択数を抽出する
     return problem_dic
 
@@ -53,7 +75,7 @@ def problem_get():
 def chunks_post(select):
     res_chunks = requests.post(URL + "/problem/chunks" + query + "&n=" + str(select), verify=False)
     alert(res_chunks)
-    print(res_chunks.text)
+    make_json("chunks.json", res_chunks.text)
     return res_chunks.text
 
 
@@ -61,8 +83,12 @@ def chunks_post(select):
 def file_get(res_chunks):
     # ファイル名配列のjsonを辞書型に変換する
     filenames = json.loads(res_chunks)
-    # audioディレクトリに移動
-    os.chdir("audio")
+    # audioディレクトリに移動、もしなければaudioディレクトリを作成し移動する
+    try:
+        os.chdir("audio")
+    except:
+        os.mkdir("audio")
+        os.chdir("audio")
     # ファイル名配列の長さ分だけfor分を回し、それぞれに対応するwavファイルをダウンロードする
     for i in range(len(filenames["chunks"])):
         # ファイル名配列のi番目にあるファイルにGETリクエストを送る
@@ -87,25 +113,45 @@ def read_res():
     # res.txtから読み込んだリストを回答形式に合うように変更してリストにする
     res_ls.sort()
     fuda_ls = []
-    for fuda in res_ls():
+    for fuda in res_ls:
         if len(fuda) == 1:
-            fuda_ls.append = "0" + fuda
+            fuda_ls.append("0" + fuda)
+    print(fuda_ls)
     return fuda_ls
+
+
+"""提出用のjsonファイルを生成"""
+def mkjson(fuda_ls):
+    # problem.txtから問題IDを読み込み
+    problem_id = ""
+    with open("problem.txt", "r") as txt:
+        problem_id = (txt.read()).replace("\n", "")
+    # 辞書型作成
+    dic = {"problem_id":problem_id, "answers":fuda_ls}
+    # jsonファイルへ書き込み
+    with open("submit.json", "w") as j:
+        json.dump(dic, j, indent=2)
+
 
 
 """/problemに回答のjsonをPOSTリクエストで送り、返ってきたjsonファイルを受け取る"""
 def problem_post():
-    res_problem = requests.post(URL + "/problem" + query, json.dump({}), headers={'Content-Type': 'application/json'}, verify=False)
-
+    with open("submit.json", "r") as submit_json:
+        res_problem = requests.post(URL + "/problem" + query,
+                                    json=json.load(submit_json),
+                                    headers={'Content-Type': 'application/json'},
+                                    verify=False)
+    make_json("response.json", res_problem.text)
 
 
 if __name__ == "__main__":
-    """最終的に音声ファイルをダウンロードする"""
+    """最終的に音声ファイルをダウンロードして結合"""
     if (sys.argv[1] == "download" or sys.argv[1] == "Download" or sys.argv[1] == "DOWNLOAD"):
         match_get()
         problem_information = problem_get()
         file_get(chunks_post(problem_information["chunks"]))
+        cat.wav_cat()
     """分析結果のjsonファイルを提出する"""
     if (sys.argv[1] == "submit" or sys.argv[1] == "Submit" or sys.argv[1] == "SUBMIT"):
-        read_res()
+        mkjson(read_res())
         problem_post()
