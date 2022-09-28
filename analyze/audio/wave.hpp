76b972b2b673953ddc7e06e635b5fc6c9ad83823
sota <sota1210nittc@gmail.com>
2022-09-28 23:24:00 +0900

    update wave.hpp

--- analyze/audio/wave.hpp
+++ analyze/audio/wave.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -15,7 +16,6 @@ constexpr int default_audio_max_length = 394606;
 constexpr int analyze_audio_max_length = default_audio_max_length / (default_sampling_hz / analyze_sampling_hz);
 static_assert(default_sampling_hz % analyze_sampling_hz == 0);
 
-//URL: https://hwswsgps.hatenablog.com/entry/2018/08/19/172401
 struct Wave {
   int fs; //サンプリング周波数
   int bits; //量子化bit数
@@ -31,105 +31,109 @@ struct Wave {
   }
 };
 
-template <typename T>
-inline void file_read(T *dst, const size_t size, FILE *fp) noexcept{
-  const size_t d = fread(dst, sizeof(T), size, fp);
-  assert(d > 0);
-}
-template <typename T>
-inline void file_write(const T *str, const size_t size, FILE *fp) noexcept{
-  const size_t d = fwrite(str, sizeof(T), size, fp);
-  assert(d > 0);
-}
+struct FileReader {
+  FILE *fp;
+  const int len;
+  char *buf;
+  const char *p;
+  FileReader(const char *file_name) : fp(fopen(file_name, "rb")), len(get_file_size(fp)), buf(new char[len]), p(buf){
+    if(fp == NULL){
+      fprintf(stderr, "file(%s) was not found", file_name);
+      exit(1);
+    }
+    const int size = (int)fread(buf, 1, len, fp);
+    assert(size == len);
+  }
+  ~FileReader(){
+    fclose(fp);
+    delete[] buf;
+  }
+  int get_file_size(FILE *fp){
+    if(fp == NULL) return 1;
+    fseek(fp, 0, SEEK_END);
+    const int pos = (int)ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    return pos;
+  }
+  constexpr void through(const int steps) noexcept{
+    p += steps;
+    assert(p < buf + len);
+  }
+  template <class T>
+  constexpr T read() noexcept{
+    assert(p < buf + len);
+    const T val = *(T*)p;
+    p += sizeof(T);
+    return val;
+  }
+};
+
+struct FileWriter {
+  FILE *fp;
+  FileWriter(const char *file_name) : fp(fopen(file_name, "wb")){
+    if(fp == NULL){
+      fprintf(stderr, "file(%s) was not found", file_name);
+      exit(1);
+    }
+  }
+  ~FileWriter(){
+    fclose(fp);
+  }
+  template <class T>
+  void write(const T val) noexcept{
+    fwrite(&val, sizeof(T), 1, fp);
+  }
+  template <size_t size>
+  void write(const char (&str)[size]) noexcept{
+    fwrite(str, 1, size-1, fp); //except null-moji
+  }
+};
 
 void read_audio(Wave &prm, const char *filename){
-  char tmp[24];
-
-  int fmt_samples_per_sec;
-  short fmt_bits_per_sample;
-  int data_size;
-
-  //wavファイルオープン
-  FILE *fp = fopen(filename, "rb");
-  assert(fp != NULL);
-
-  //wavデータ読み込み
-  file_read(tmp, 24, fp);
-  file_read(&fmt_samples_per_sec, 1, fp);
-  file_read(tmp, 6, fp);
-  file_read(&fmt_bits_per_sample, 1, fp);
-  file_read(tmp, 4, fp);
-  file_read(&data_size, 1, fp);
-
-  //パラメータ代入
-  prm.fs = fmt_samples_per_sec;
-  prm.bits = fmt_bits_per_sample;
-  prm.L = data_size / 2;
-
-  //音声データ代入
+  FileReader reader(filename);
+  reader.through(24);
+  prm.fs = reader.read<int>();
+  reader.through(6);
+  prm.bits = (int)reader.read<short>();
+  reader.through(4);
+  prm.L = reader.read<int>() / 2;
   prm.data.resize(prm.L);
   for(int i = 0; i < prm.L; i++){
-    short data_data;
-    file_read(&data_data, 1, fp);
-    prm[i] = data_data;
+    prm[i] = (int)reader.read<short>();
   }
-  fclose(fp);
 }
+
 void write_audio(const Wave &prm, const char *filename){
-  FILE *fp = fopen(filename, "wb");
-  assert(fp != NULL);
-
-  //ヘッダー書き込み
-  const int header_size = 36 + prm.L * 2;
-
-  file_write("RIFF", 4, fp);
-  file_write(&header_size, 1, fp);
-  file_write("WAVE", 4, fp);
-  file_write("fmt ", 4, fp);
-
-  const int fmt_size = 16;
-  const short fmt_format = 1;
-  const short fmt_channel = 1;
-  const int fmt_samples_per_sec = prm.fs;
-  const int fmt_bytes_per_sec = prm.fs * prm.bits / 8;
-  const short fmt_block_size = prm.bits / 8;
-  const short fmt_bits_per_sample = prm.bits;
-  file_write(&fmt_size, 1, fp);
-  file_write(&fmt_format, 1, fp);
-  file_write(&fmt_channel, 1, fp);
-  file_write(&fmt_samples_per_sec, 1, fp);
-  file_write(&fmt_bytes_per_sec, 1, fp);
-  file_write(&fmt_block_size, 1, fp);
-  file_write(&fmt_bits_per_sample, 1, fp);
-
-  //データ書き込み
-  const int data_size = prm.L * 2;
-  file_write("data", 4, fp);
-  file_write(&data_size, 1, fp);
-
-  //音声データ書き込み
+  FileWriter writer(filename);
+  writer.write("RIFF");
+  writer.write(36 + prm.L * 2);
+  writer.write("WAVEfmt ");
+  writer.write(16);
+  writer.write((short)1);
+  writer.write((short)1);
+  writer.write(prm.fs);
+  writer.write(prm.fs * prm.bits / 8);
+  writer.write((short)(prm.bits / 8));
+  writer.write((short)prm.bits);
+  writer.write("data");
+  writer.write(prm.L * 2);
   for(int i = 0; i < prm.L; i++){
     short data_data;
-    //リミッター
     if(prm[i] > short_max) data_data = short_max;
     else if(prm[i] < -short_max) data_data = -short_max;
     else data_data = prm[i];
-    fwrite(&data_data, 2, 1, fp);
+    writer.write(data_data);
   }
-  fclose(fp);
 }
 
 
 //prm.fs[Hz] -> fs[Hz]
-void change_sampling_hz(Wave &prm, int fs){
+void change_sampling_hz(Wave &prm, const int fs){
   assert(prm.fs % fs == 0);
   const int p = prm.fs / fs;
   prm.fs = fs;
   std::vector<int> new_data(prm.L / p);
   for(int i = 0; i < prm.L / p; i++){
-    //int tot = 0;
-    //for(int j = 0; j < p; j++) tot += prm[i*p + j];
-    //new_data[i] = tot / p;
     new_data[i] = prm[i * p];
   }
   prm.L /= p;
@@ -144,7 +148,7 @@ Wave merge_audio(const std::vector<Wave> &waves, const std::vector<int> &st){
   res.bits = waves[0].bits;
   res.fs = waves[0].fs;
   res.L = 0;
-  const int n = waves.size();
+  const int n = (int)waves.size();
   for(int i = 0; i < n; i++){
     assert(res.bits == waves[i].bits && res.fs == waves[i].fs);
     res.L = std::max(res.L, waves[i].L + st[i]);
@@ -165,7 +169,7 @@ Wave merge_audio(const std::vector<Wave> &waves, const std::vector<int> &st){
 //sep[i]: 分割する位置。sep.front() = 0, sep.back() = wave.L
 [[nodiscard]]
 std::vector<Wave> separate_audio(const Wave &wave, const std::vector<int> &sep){
-  const int n = sep.size();
+  const int n = (int)sep.size();
   assert(sep[0] == 0 && sep[n - 1] == wave.L);
   for(int i = 1; i < n - 1; i++){
     assert(sep[i] < sep[i + 1]);
